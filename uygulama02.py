0cd0114fe2215b09883d5f6f9549754f3dd1e109
serenkoker <36674252+serenkoker@users.noreply.github.com>
2018-02-27 21:54:21 +0300

    Add files via upload

---
+++ uygulama02.py
@@ -0,0 +1,18 @@
+saniye= 96537869
+
+print("saniye =" + str(saniye))
+
+dakika= (saniye/60)
+print("dakika=" + str(dakika))
+
+saat= (dakika/60)
+print("saat= "+ str(saat))
+
+gün= (saat/24)
+print("gün=" + str(gün))
+
+ay= (gün/30)
+print("ay =" + str (ay))
+
+yıl= (ay/12)
+print("yıl=" + str(yıl))
